a75feeeb55f7a04ace6156705c284846fbf28a52
cassidybeni <cassidybeni@pursuit.org>
2022-12-10 12:33:59 -0500

    Delete boilerplate code

--- back-end/app.js
+++ back-end/app.js
@@ -10,27 +10,10 @@ app.use(cors());
 app.use(express.json()); // Parse incoming JSON
 
 // ROUTES
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
 
-/////////////////////////////////////
-// REMOVE AFTER SUCCESSFUL DEPLOYMENT
-/////////////////////////////////////
+
 const db = require("./db/dbConfig.js");
 
-app.get("/test", async (req, res) => {
-  try {
-    const allDays = await db.any("SELECT * FROM test");
-    res.json(allDays);
-  } catch (err) {
-    res.json(err);
-  }
-});
-
-/////////////////////////////////////
-// REMOVE AFTER SUCCESSFUL DEPLOYMENT
-/////////////////////////////////////
 
 // EXPORT
 module.exports = app;
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,10 +1,1 @@
-\c template_test_dev;
 
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
