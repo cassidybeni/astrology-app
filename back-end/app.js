ac6be4e733385f6d3b0a09163a3040cbed469adc
cassidybeni <cassidybeni@pursuit.org>
2022-12-11 22:07:37 -0500

    Add home route and install express

--- back-end/app.js
+++ back-end/app.js
@@ -10,10 +10,15 @@ app.use(cors());
 app.use(express.json()); // Parse incoming JSON
 
 // ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to the Astrology App!");
+});
 
+app.listen(3003, () => {
+  console.log("Listening for requests on port 3003");
+});
 
 const db = require("./db/dbConfig.js");
-
 
 // EXPORT
 module.exports = app;
